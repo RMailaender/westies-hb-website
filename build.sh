205a9cde7b177cc54c814eacfeccefba10316b88
Rene Mailaender <r.89.mailaender@gmail.com>
2024-01-26 17:18:03 +0100

    add seed to build script

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ roc format ./src/server.roc
 roc build ./src/server.roc --optimize --output ./build/server
 
 sqlite3 build/data/data.db ".read db/init_db.sql" 
+sqlite3 build/data/data.db ".read db/seed.sql" 
