9c713b9baa325f3187f03705c2853439bb1e5955
Rene Mailaender <r.89.mailaender@gmail.com>
2024-02-02 14:53:14 +0100

    change build script

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ roc format ./src/server.roc
 
 roc build ./src/server.roc --linker=legacy --output ./build/server
 
-sqlite3 build/data/data.db ".read db/init_db.sql" 
-sqlite3 build/data/data.db ".read db/seed.sql" 
+sqlite3 build/data.db ".read db/init_db.sql" 
+sqlite3 build/data.db ".read db/seed.sql" 
